9bfc16208df806fc28a350d015434b9025bd1a46
rinabo <rinaneman96@gmail.com>
2023-04-01 23:56:36 -0500

    Modify js_ver to make it able to use action.js

--- js_ver/public/action.js
+++ js_ver/public/action.js
@@ -1,17 +1,12 @@
 //jshint esversion:6
 
-module.exports = getDetails;
 
-function getDetails(){
 
-  $(".square").on('mouseenter', function(e){
-      $(this).find("img").hide();
-      $(this).find(".btn-info").show();
-  }).on('mouseleave', function(e){
-      $(this).find(".btn-info").hide();
-      $(this).find("img").show();
-  });
+$(".square").on('mouseenter', function(e){
+  $(this).find("img").hide();
+  $(this).find(".btn-info").show();
+}).on('mouseleave', function(e){
+  $(this).find(".btn-info").hide();
+  $(this).find("img").show();
+});
 
-};
-
-console.log(module);
